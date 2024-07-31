f2b2e71f3cab63e00071b28a6a86202a403139ed
Skycap <skycap@Navdeep.local>
2024-07-31 11:46:25 +0530

    feat: File upload from frontend to mongdb

--- server/models/eventsModel.ts
+++ server/models/eventsModel.ts
@@ -5,6 +5,12 @@ const eventSchema = new Schema(
     // user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
     title: { type: String, required: true },
     description: { type: String },
+    images: [
+      {
+        originalname: { type: String },
+        path: { type: String },
+      },
+    ],
   },
   {
     timestamps: true,
