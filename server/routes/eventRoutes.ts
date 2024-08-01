338281ce046b01272cef9dc70bd5a46dff0080e1
Skycap <skycap@Navdeep.local>
2024-08-01 11:07:26 +0530

    feat: SSE implementation

--- server/routes/eventRoutes.ts
+++ server/routes/eventRoutes.ts
@@ -1,12 +1,23 @@
 import express from "express";
 import multer from "multer";
-import { createEvents, getEvents } from "../controllers/eventControllers";
+import {
+  createEvents,
+  getEventDetails,
+  getEvents,
+  resetNotificationCount,
+  sseEvents,
+} from "../controllers/eventControllers";
 import { storage } from "../utils/multerStorage";
 
 const upload = multer({ storage });
 
 const route = express.Router();
+
+route.get("/stream", sseEvents);
+route.post("/reset-notification", resetNotificationCount);
 route.post("/create", upload.array("images", 5), createEvents);
 route.get("/get", getEvents);
+route.get("/:eventId", getEventDetails);
+
 
 export default route;
